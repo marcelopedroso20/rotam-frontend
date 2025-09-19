779cb08f22e1ab312b77a3c32e97e76469c7ef91
marcelopedroso20 <68392028+marcelopedroso20@users.noreply.github.com>
2025-09-19 15:23:08 -0400

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+self.addEventListener("install", event => {
+  console.log("Service Worker instalado");
+  event.waitUntil(
+    caches.open("rotam-cache").then(cache => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./manifest.json"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
