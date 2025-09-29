442fe3012af08596b477ac64f808d67e76ab9312
marcelopedroso20 <68392028+marcelopedroso20@users.noreply.github.com>
2025-09-29 17:19:47 -0400

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,1 +1,16 @@
-'./js/Auth.js'
+const PRECACHE_URLS = [
+  './',
+  './index.html',
+  './login.html',
+  './cadastro.html',
+  './relatorios.html',
+  './assets/style.css',
+  './assets/logo-rotam-frontend.png',
+  './assets/logo-rotam-bg-1920x1080.png',
+  './assets/logo-rotam-192.png',
+  './assets/logo-rotam-512.png',
+  './js/login.js',
+  './js/logout.js',
+  './js/pwa.js',
+  './manifest.json'
+];
